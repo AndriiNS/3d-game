f7d515d7a5cfe5c2650c5791416cf01fa8aa191d
AndriiNS <andriy.naumov05@gmail.com>
2024-09-18 21:09:57 +0300

    add detail

--- src/js/index.js
+++ src/js/index.js
@@ -39,3 +39,10 @@ document.addEventListener("DOMContentLoaded", () => {
     });
   });
 });
+
+function setFullHeight() {
+  document.documentElement.style.setProperty("--vh", `${window.innerHeight * 0.01}px`);
+}
+
+window.addEventListener("resize", setFullHeight);
+setFullHeight();
