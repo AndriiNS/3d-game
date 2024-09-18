b29076beaf8d1446752bda0759ed339b8db37464
AndriiNS <andriy.naumov05@gmail.com>
2024-09-18 22:31:15 +0300

    .

--- src/js/index.js
+++ src/js/index.js
@@ -47,3 +47,10 @@ document.addEventListener("DOMContentLoaded", () => {
     soundButton.classList.toggle("muted");
   });
 });
+function setFullHeight() {
+  const vh = window.innerHeight * 0.01;
+  document.documentElement.style.setProperty("--vh", `${vh}px`);
+}
+
+window.addEventListener("resize", setFullHeight);
+setFullHeight();
